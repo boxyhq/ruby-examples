7f65a78a7fac6d4c3bafabb075818bd24c16d522
Aswin V <vaswin91@gmail.com>
2022-10-24 23:45:54 +0530

    app routes

--- rails-enterprise-sso/config/routes.rb
+++ rails-enterprise-sso/config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   root 'home#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/sso', to: 'sso#index'
+  get 'sso', to: 'logins#index'
+  post 'sso', to: 'oauths#oauth'
+  resource :oauth do
+    get :callback, to: 'oauths#callback', on: :collection
+  end
+
+  get 'profile', to: 'profiles#index', as: 'profile'
 end
