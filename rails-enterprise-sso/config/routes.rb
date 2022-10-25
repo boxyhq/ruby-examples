3cad6b2b3c3346a94a8b672874444e3738496bd5
Aswin V <vaswin91@gmail.com>
2022-10-25 12:50:06 +0530

    Add logout feature

--- rails-enterprise-sso/config/routes.rb
+++ rails-enterprise-sso/config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'sso', to: 'logins#index', as: 'login'
   post 'sso', to: 'oauths#oauth'
+
+  delete 'logout' => 'logins#destroy', as: :logout
+
   resource :oauth do
     get :callback, to: 'oauths#callback', on: :collection
   end
