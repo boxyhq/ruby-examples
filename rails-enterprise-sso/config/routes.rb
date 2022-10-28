5ff3db57811d72b89081d0905e7203d53665ac22
Aswin V <vaswin91@gmail.com>
2022-10-28 22:24:23 +0530

    Fix route names

--- rails-enterprise-sso/config/routes.rb
+++ rails-enterprise-sso/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'home#index'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'sso', to: 'logins#index', as: 'login'
+  get 'sso', to: 'logins#index', as: :login
   post 'sso', to: 'oauths#oauth'
 
   delete 'logout' => 'logins#destroy', as: :logout
@@ -11,8 +11,8 @@ Rails.application.routes.draw do
     get :callback, to: 'oauths#callback', on: :collection
   end
 
-  get 'profile', to: 'profiles#index', as: 'profile'
+  get 'profile', to: 'profiles#index', as: :profile
 
-  get '/setup/sso', to: 'ssos#index', as: 'setup_sso'
-  post '/setup/sso', to: 'ssos#save_connection', as: 'save_connection'
+  get '/setup/sso', to: 'ssos#index', as: :setup_sso
+  post '/setup/sso', to: 'ssos#save_connection', as: :save_connection
 end
