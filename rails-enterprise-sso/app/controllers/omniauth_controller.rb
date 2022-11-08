700da6ff85b80828845979ccd8c98cd4e9bb992e
Aswin V <vaswin91@gmail.com>
2022-11-08 22:54:10 +0530

    Rename omniauth controller and add logout action

--- rails-enterprise-sso/app/controllers/omniauth_controller.rb
+++ rails-enterprise-sso/app/controllers/omniauth_controller.rb
@@ -1,9 +1,14 @@
-class SessionsController < ApplicationController
+class OmniauthController < ApplicationController
   skip_before_action :require_login, raise: false
 
     def callback
       user_info = request.env['omniauth.auth']
       session[:userinfo] = user_info['extra']['raw_info']
       redirect_to omniauth_profile_path,  notice: "Logged in using omniauth!"
+    end
+
+    def logout
+      reset_session
+      redirect_to root_path,  notice: "Logged out from Omniauth!"
     end
   end
