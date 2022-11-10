52cdcd598a215156417028b3879392c25914637c
Aswin V <vaswin91@gmail.com>
2022-11-10 16:21:45 +0530

    Tweak comment

--- rails-enterprise-sso/app/lib/sorcery/providers/boxyhqsso.rb
+++ rails-enterprise-sso/app/lib/sorcery/providers/boxyhqsso.rb
@@ -1,6 +1,6 @@
 module Sorcery
   module Providers
-    # This class adds support for OAuth with BoxyHQ SAML.
+    # This class adds support for OAuth2.0 SSO with Jackson service.
     #
     #   config.boxyhqsso.site = <http://localhost:5225>
     #   config.boxyhqsso.key = <key>
